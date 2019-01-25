ecdfcebbe6d58a56bbfe8ff7d516ae5a0a6cba28
Bzzbzik <bzzbzik@gmail.com>
2019-01-25 09:03:22 +0400

    add new folder

---
+++ onetohundred/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void numerate(int max, int step) {
+        int i = 0;
+        while (i <= max) {
+            System.out.println(i);
+            i = i + step;
+        }
+    }
+
+    public static void main(String[] args) {
+        numerate(100,1);
+        whileloop loop = null;
+        loop.tillten();
+    }
+}
+
+ class whileloop {
+    public static void tillten() {
+      int n;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input '10' number: ");
+        n = input.nextInt();
+        while (n !=10) {
+            System.out.println("You entered: " + n);
+            System.out.println("Please input '10': ");
+            n = input.nextInt();
+
+        }
+        System.out.println("Thank you!");
+    }
+}
